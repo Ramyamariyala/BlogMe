24f56e0d0d42856493b391f4f6dcddeec63457e4
Trill Ville <computersleek@gmail.com>
2021-03-30 20:09:27 -0400

    css edit

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
-{
+require("dotenv").config();
+module.exports=  {
   "development": {
-    "username": "root",
-    "password": "Rutgers",
-    "database": "blog_me",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
